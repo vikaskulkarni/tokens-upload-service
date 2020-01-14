760343a3cee51ff2d50b4de4b09f340b7e698f0d
Vikas K <vikas.kulkarni@gmail.com>
2020-01-14 10:32:44 -0700

    removed warnings

--- src/main/java/com/tokens/api/ApiException.java
+++ src/main/java/com/tokens/api/ApiException.java
@@ -2,10 +2,16 @@ package com.tokens.api;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-12-18T15:17:56.845-07:00")
 
-public class ApiException extends Exception{
-    private int code;
-    public ApiException (int code, String msg) {
-        super(msg);
-        this.code = code;
-    }
+public class ApiException extends Exception {
+	private static final long serialVersionUID = 5265177832668512670L;
+	private int code;
+
+	public ApiException(int code, String msg) {
+		super(msg);
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
 }
